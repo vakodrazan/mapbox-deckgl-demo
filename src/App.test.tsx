ebc6114c7192b4bf7bac3baaec94110fd22bfb3e
noeline <noeline.mar@onja.org>
2022-06-08 16:40:10 +0300

    add testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import 'expect-puppeteer'
+import puppeteer from "puppeteer"
+describe('Load Button', () => {
+  test('Button loads correctly', async () => {
+    let browser = await puppeteer.launch({
+      headless: false
+    });
+    let page = await browser.newPage();
+    page.emulate({
+      viewport: {
+        width: 500,
+        height: 2400
+      },
+      userAgent: ''
+    });
+    await page.goto('http://localhost:3000/');
+    await page.waitForSelector('.MuiButton-root');
+    const html = await page?.$eval('.MuiButton-root', (e: any) => e.innerHTML);
+    expect(html).toBe('Load GeoJson<span class=\"MuiTouchRipple-root css-8je8zh-MuiTouchRipple-root\"></span>');
+    browser.close();
+  }, 16000);
 });
